e34513089695bbb0cfa061148adcc798d8b9a15d
Bernhard Häussermann <bernhardgh@icloud.com>
2022-04-27 21:22:20 +0200

    Add employee filtering

--- Northwind.Client/Pages/Employees.razor.cs
+++ Northwind.Client/Pages/Employees.razor.cs
@@ -6,15 +6,33 @@ namespace Northwind.Client.Pages;
 
 public partial class Employees
 {
-    public IEnumerable<Employee> EmployeesList { get; private set; } = Array.Empty<Employee>();
+    private IEnumerable<Employee> employees;
+
+    public IEnumerable<Employee> FilteredEmployeesList
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                return this.employees;
+            }
+
+            return this.employees.Where(e =>
+                e.LastName.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase)
+                || e.FirstName.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase)
+                || e.Title.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
 
     [Inject]
     private EmployeeDataService EmployeeDataService { get; set; }
 
+    public string SearchText { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
-        EmployeesList = (await EmployeeDataService.GetAll())
+        this.employees = (await EmployeeDataService.GetAll())
             .OrderBy(e => e.LastName)
             .ToArray();
     }
